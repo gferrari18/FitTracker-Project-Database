1e07eaa96312915baf4b4eee37a5dbb3e22fdb97
gferrari18 <99638323+gferrari18@users.noreply.github.com>
2022-03-18 12:38:34 -0700

    Update trial.py

--- trial.py
+++ trial.py
@@ -5,5 +5,7 @@ if usertypeUP != "YES":
                  print("Hello, " + usertype + ". I will create your profile")
                  f = open(usertypeUP + ".txt", "w")
                  f.write(usertype + "/n")
- 
+else:
+                 f = open(usertypeUP + ".txt", "r")
+                 print("Welcome back, " + f.print(firstline()))
    
