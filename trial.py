25059d469d0c42fb4d3069df2668cf5faad1b98e
gferrari18 <99638323+gferrari18@users.noreply.github.com>
2022-03-18 12:17:18 -0700

    Update trial.py

--- trial.py
+++ trial.py
@@ -2,5 +2,8 @@ usertype = input("Have you registered before? (type Yes or No)
 usertypeUP = usertype.upper()
                 
 if usertypeUP != "YES":
-  f = open(usertypeUP.txt, 
+                 print("Hello, " + usertype + ". I will create your profile")
+                 f = open(usertypeUP + ".txt", "w")
+                 f.write(usertype + "/n")
+ 
    
