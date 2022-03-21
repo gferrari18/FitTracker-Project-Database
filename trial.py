3e9abc19255004f72ca5da0fdb46ef1db861609f
gferrari18 <gferrari18@my.whitworth.edu>
2022-03-20 21:08:19 -0700

    started file handler

--- trial.py
+++ trial.py
@@ -1,11 +1,51 @@
-usertype = input("Have you registered before? (type Yes or No)
+import os
+
+usertype = input("Have you registered before? (type Yes or No): ")
 usertypeUP = usertype.upper()
-                
-if usertypeUP != "YES":
-                 print("Hello, " + usertype + ". I will create your profile")
-                 f = open(usertypeUP + ".txt", "w")
-                 f.write(usertype + "/n")
-else:
-                 f = open(usertypeUP + ".txt", "r")
-                 print("Welcome back, " + f.print(firstline()))
-   
+
+while usertypeUP != "YES" and usertypeUP != "NO": #loop created to ensure user types either yes or no
+    print("Sorry, I do not understand. Please type Yes or No")
+    usertype = input("Have you registered before? (type Yes or No): ")
+    usertypeUP = usertype.upper()
+
+if usertypeUP == "YES":
+    user = input("Please enter your name: ")
+    userUP = user.upper()
+    f = open(userUP + ".txt", "a") #if start with "r", will give an error if file does not exist
+    f.close()
+    f = open(userUP + ".txt", "r")
+    while f.readline() == "": #loop created to ensure file exists. loop will exit once an user with the name of a created file is typed.
+        print("This name does not seem to be registered. Try rewriting your name.")
+        f.close()
+        os.remove(userUP + ".txt")
+        user = input("Please enter your name: ")
+        userUP = user.upper()
+        f = open(userUP + ".txt", "a")
+        f.close
+        f = open(userUP + ".txt", "r")
+    f.close()
+    print("Welcome back, " + user.capitaliza() + ".")
+
+                                 
+elif usertypeUP == "NO": #creates file for new user
+    user = input("Please enter your name: ")
+    userUP = user.upper()
+    f = open(userUP + ".txt", "a")
+    f.close()
+    f = open(userUP + ".txt", "r")
+    while f.readline() != "": #this loop ensure another file with the same name does not exist
+        f.close()
+        print("This name is already in use. Please use a different one.")
+        user = input("Please enter your name: ")
+        userUP = user.upper()
+        f = open(userUP + ".txt", "a")
+        f.close()
+        f = open(userUP + ".txt", "r")
+    f.close()
+    f = open(userUP + ".txt", "a")
+    f.write(user + "\n")
+
+    print("Welcome, " + user.capitalize() + ".")
+
+
+
