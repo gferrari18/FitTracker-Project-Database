2a82d0638a1a67c6c10f44f3867269a537f39329
gferrari18 <gferrari18@my.whitworth.edu>
2022-03-21 19:37:16 -0700

    nice

---
+++ hm.py
@@ -0,0 +1,9 @@
+train = ""
+
+while train != "DONE":
+    train = input("Enter an exercise you plan to do, or type DONE when you have added all desired exercises for a day ")
+    if train != "DONE":
+        set = input("enter how many sets you plan to do: ")
+        reps = input("Enter how many reps you plan to do: ")
+        weight = input("enter how much weight you plan to do, or how much weight you have lifted: ")
+
