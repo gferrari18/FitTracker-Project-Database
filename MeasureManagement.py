545879a4a96a035c0a63d22c80985ee1e7a3d4e2
gferrari18 <gferrari18@my.whitworth.edu>
2022-03-23 20:32:57 -0700

    finished

--- MeasureManagement.py
+++ MeasureManagement.py
@@ -1,18 +1,52 @@
 import os
 
-def num(text):
-    while text.isnumeric() == False:
+def num(p):    #Function created to ensure user enters a number instead of other characters
+    text = input(p)
+    while text[0].isnumeric() == False:
         print("You must enter a number.")
-        text = input("Enter your " + text + "using numbers")
+        text = input(p)
+    return text
+
+def reg(n,m): #Function created to display past user entries
+    f = open(userUP + ".txt", "r")
+    if os.path.getsize(userUP + ".txt") > 0:
+        print("Your previous " + n + " measurements were: ")
+    for line in f:
+        if line.startswith(m):
+            print(line[2:].strip())
+        else: continue
+    f.close()
+
+
+def app(m, n):
+    f = open(userUP + ".txt", "a")
+    f.write(m + n + "\n")
+    f.close()
+
+def average(m, n, o):
+    f = open(userUP + ".txt", "r")
+    avg = 0
+    hm = 0
+    for line in f:
+        if line.startswith(n):
+            hm = hm + float(line[2:].strip())
+            avg = avg + 1
+    resavg = hm / avg
+    avgtot = (float(o) - float(resavg))
+    print("Your " + m + " measurement average is: " + str(resavg))
+    print("Compared to your average, your " + m + " measurement difference is: " + str(avgtot))
+    f.close()
+
 
 
 usertype = input("Have you registered before? (type Yes or No): ")
 usertypeUP = usertype.upper()
 
 while usertypeUP != "YES" and usertypeUP != "NO": #loop created to ensure user types either yes or no
-    print("Sorry, I do not understand. Please type Yes or No")
+    print("Sorry, I do not understand. Please type Yes, No, or back to return to the previous menu")
     usertype = input("Have you registered before? (type Yes or No): ")
     usertypeUP = usertype.upper()
+
 
 if usertypeUP == "YES":
     user = input("Please enter your name: ")
@@ -48,31 +82,24 @@ elif usertypeUP == "NO": #creates file for new user
         f.close()
         f = open(userUP + ".txt", "r")
     f.close()
-    f = open(userUP + ".txt", "a")
-    f.write(user + "\n")
-
+   
     print("Welcome, " + user.capitalize() + ".")
 
+weight = num("Please enter your weight: ")
+reg("weight", "1-")
+waist = num("Please enter your waist measurement: ")
+reg("waist", "2-")
+arms = num("Enter your arm (biceps/triceps) measurement: ")
+reg("arms", "2-")
+thighs = num("Enter your thigh measurement: ")
+reg("thighs", "4-")
 
-weight = input("Please enter your weight: ")
-while weight.isalpha():
-    print("You must enter a number.")
-    weight = input("Enter it now using numbers: ")
+app("1-",weight)
+app("2-",waist)
+app("3-",arms)
+app("4-",thighs)
 
-waist = input("Please enter your waist measurement: ")
-while waist.isalpha():
-    print("You must enter a number.")
-    waist = input("Enter it now using numbers: ")
-
-arms = input("Enter your arm (biceps/triceps) measurement: ")
-while arms.isalpha():
-    print("You must enter a number.")
-    arms = input("Enter it now using numbers: ")
-
-thighs = input("Enter your thigh measurement: ")
-while thighs.isalpha():
-    print("You must enter a number.")
-    thighs = input("Enter it now using numbers: ")
-
-measurements = (weight, waist, arms, thighs) #placing measurements into a tuple
-
+average("weight", "1-",weight )
+average("waist", "2-",waist)
+average("arms", "3-",arms)
+average("thighs", "4-",thighs)
