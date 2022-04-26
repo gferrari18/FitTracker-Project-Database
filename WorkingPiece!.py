3dfc761adc61cb98995aeaa4b770857deb1c4a33
gferrari18 <gferrari18@my.whitworth.edu>
2022-04-25 22:07:17 -0700

    User manager working

--- WorkingPiece!.py
+++ WorkingPiece!.py
@@ -2,6 +2,7 @@ from Main import Ui_MainWindow
 from nonregusrscreen import Ui_NonRegUserScreen
 from PyQt5 import QtCore, QtGui, QtWidgets
 from reguserscreen import Ui_RegUserScreen
+import os
 
 class Firstwindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
@@ -16,6 +17,19 @@ class NonRegwindow(QtWidgets.QDialog, Ui_NonRegUserScreen):
         super(NonRegwindow, self).__init__(parent)
         self.setupUi(self)
         self.pushButton_2.clicked.connect(self.hide)
+    
+    def register(self):
+        user = self.lineEdit.text()
+        userUP = user.upper()
+        f = open(userUP + ".txt", "a") #if start with "r", will give an error if file does not exist
+        f.close()
+        f = open(userUP + ".txt", "r")
+        if f.readline() != "": #this ensures another file with the same name does not exist
+            self.entername.setText("This name is already in use. Try another one")
+            f.close()
+
+        elif f.readline() == "":
+            print("Welcome, " + user.capitalize() + ".")
 
 
 class Regwindow(QtWidgets.QDialog, Ui_RegUserScreen):
@@ -24,11 +38,25 @@ class Regwindow(QtWidgets.QDialog, Ui_RegUserScreen):
         self.setupUi(self)
         self.pushButton_2.clicked.connect(self.hide)
 
+    def openuser(self):
+        user = self.lineEdit.text()
+        userUP = user.upper()
+        f = open(userUP + ".txt", "a") #if start with "r", will give an error if file does not exist
+        f.close()
+        f = open(userUP + ".txt", "r")
+        if f.readline() == "":
+            self.entername.setText("This name does not seem to be registered.")
+            f.close()
+            os.remove(userUP + ".txt")
+            
+        else: print("Mahoe")            
 
-
+    
 
 class Manager:
     def __init__(self):
+
+        #Show windows/hide windows section
         self.first = Firstwindow()
         self.second = NonRegwindow()
         self.third = Regwindow()
@@ -37,9 +65,13 @@ class Manager:
         self.first.pushButton_2.clicked.connect(self.second.show)
         self.second.pushButton_2.clicked.connect(self.first.show)
         self.third.pushButton_2.clicked.connect(self.first.show)
-
         self.first.show()
 
+        #linked to functions related to NonReg window
+        self.second.pushButton.clicked.connect(self.second.register)
+
+        #linked to functions related to Reg window
+        self.third.pushButton.clicked.connect(self.third.openuser)
 
 if __name__ == '__main__':
     import sys
